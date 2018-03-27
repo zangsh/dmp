33de7003c140def745ccd13c14188f62e5855b0c
zangsh <379624425@qq.com>
2018-03-27 14:59:39 +0800

    过滤器返回统一格式操作信息

--- src/main/java/com/mw/dmp/DmpApplication.java
+++ src/main/java/com/mw/dmp/DmpApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * 大数据管理平台
