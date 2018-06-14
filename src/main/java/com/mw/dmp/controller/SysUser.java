ef743e406ad22e6b07ea4a8381ea2e6aaebd2017
zangsh <zshuaihu@czbapp.com>
2018-06-14 11:36:05 +0800

    整合apidoc

--- src/main/java/com/mw/dmp/controller/SysUser.java
+++ src/main/java/com/mw/dmp/controller/SysUser.java
@@ -32,9 +32,23 @@ public class SysUser {
     private RedisUtils redisUtils;
 
     /**
-     * 用户登陆校验获取相应信息
-     * @param json
-     * @return
+     * @api {post} http://localhost:8080/user/postLogin 用户登陆
+     * @apiName 用户登陆
+     * @apiGroup user-interface
+     * @apiVersion 0.0.1
+     * @apiDescription 用户登陆校验获取相应信息
+     * @apiDescription 用于注册用户
+     * @apiParam {String} account 用户账户名
+     * @apiParam {String} password 密码
+     * @apiParam {String} mobile 手机号
+     * @apiParam {int} vip = 0  是否注册Vip身份 0 普通用户 1 Vip用户
+     * @apiParam {String} [recommend] 邀请码
+     * @apiParamExample {json} 请求样例：
+     *                ?account=sodlinken&password=11223344&mobile=13739554137&vip=0&recommend=
+     * @apiSuccess (200) {String} msg 信息
+     * @apiSuccess (200) {int} code 0 代表无错误 1代表有错误
+     * @apiSuccessExample {json} 返回样例:
+     *                {"code":"0","msg":"注册成功"}
      */
     @ResponseBody
     @RequestMapping(value = "/postLogin",method = RequestMethod.POST,produces = MediaType.APPLICATION_JSON_VALUE)
